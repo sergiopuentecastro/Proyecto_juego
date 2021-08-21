dc8a04cf76d7e0b62a770b9e85e7c19f60cecdda
Laura <lauradecc@gmail.com>
2021-08-21 18:17:27 +0200

    first map

--- js/background.js
+++ js/background.js
@@ -1,15 +1,24 @@
 class Square {
-    constructor(ctx, x, y, width = 50, height = 50) {
+    constructor(ctx, x, y) {
         this.ctx = ctx;
-        this.width = width;
-        this.height = height;
+        this.width = 50;
+        this.height = 50;
         this.color = "black";
         this.x = x;
         this.y = y;
     }
+
     draw() {
-        let ctx = game.context;
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        //ctx.fillStyle = this.color;
+        this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    checkCollition(character) {
+        if(((this.x > character.x)
+          && this.x < character.x + character.width) 
+          && ((this.y > character.y || this.y + this.height > character.y)
+          && this.y < character.y + character.height)) {
+          this.speedX *= -1
+        }
     }
 }
--- js/game.js
+++ js/game.js
@@ -8,16 +8,60 @@ const game = {
     init(id) {
         this.canvas = document.getElementById(id);
         this.ctx = this.canvas.getContext('2d');
-        //this.width = 1000;
-        //this.height = 600;
         console.log('entro en init')
-        square1 = new Square(this.ctx, 0, 0);
+        /* Todas estas funciones se pueden meter en 
+        la clase cuadrado o reducirlo de alguna manera fuera
+        de game??? */
+        this.bordersTopBottom(20, 550, 50)
+        this.bordersLeftRight(10, 950, 50)
+        this.interiorMap()
         //this.setMap();
         //this.start();
     },
     //start()
-    setMap() {
-        console.log('entro en setMap')
-        square1 = new Square(this.ctx, 0, 0);
+    createMap() {
+        this.bordersTopBottom(20, 550, 50)
+        this.bordersLeftRight(10, 950, 50)
+        this.interiorMap()
+    },
+
+    bordersTopBottom(squaresNum, space, squareSize) {
+        // Se podría sustituir squareSize por el tamaño de la clase?
+        let borderTop = [];
+        let borderBottom = [];
+        for (let i = 0; i < squaresNum; i++) {
+            borderTop.push(new Square(this.ctx, [i * squareSize], 0));
+            borderTop[i].draw();
+            borderBottom.push(new Square(this.ctx, [i * squareSize], space));
+            borderBottom[i].draw();
+        }
+    },
+
+    bordersLeftRight(squaresNum, space, squareSize) {
+        let borderLeft = [];
+        let borderRight = [];
+        for (let i = 0; i < squaresNum; i++) {
+            borderLeft.push(new Square(this.ctx, 0, [(i + 1) * squareSize]));
+            borderLeft[i].draw();
+            borderRight.push(new Square(this.ctx, space, [(i + 1) * squareSize]));
+            borderRight[i].draw();
+        }
+    },
+
+    verticalBar(squaresNum, x, y, squareSize) {
+        let verticalBar = [];
+        for (let i = 0; i < squaresNum; i++) {
+            verticalBar.push(new Square(this.ctx, x, y + squareSize * i));
+            verticalBar[i].draw();
+        }
+    },
+
+    interiorMap() {
+        this.verticalBar(8, 150, 50, 50)
+        this.verticalBar(8, 300, 150, 50)
+        this.verticalBar(8, 450, 50, 50)
+        this.verticalBar(8, 600, 150, 50)
+        this.verticalBar(8, 750, 50, 50)
     }
+
 }
